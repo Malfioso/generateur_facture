e40d09bdcd543372bc7164819348b020bfeba753
Jeremie <jeremsxm97@gmail.com>
2025-07-16 19:29:54 +0200

    feat : creation of a model product

--- invoice/products/views.py
+++ invoice/products/views.py
@@ -2,9 +2,19 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from products.models import Product
 
 def products_list(request):
     return HttpResponse('<h1>PAGE DES PRODUITS</h1>')
 
+
 def about(request):
-    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+    products = Product.objects.all() 
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes fruits préférés sont :<p>
+        <ul>
+            <li>{products[0].name}</li>
+            <li>{products[1].name}</li>
+        </ul>
+""")

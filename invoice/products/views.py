ac50a664d089fe1fc9508e2ee316ccab8d8b9850
Jeremie <jeremsxm97@gmail.com>
2025-07-16 15:52:53 +0200

    feat : creation of app products and invoices

---
+++ invoice/products/views.py
@@ -0,0 +1,7 @@
+# ~/projects/generateur_facture/invoice/products/views.py
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
